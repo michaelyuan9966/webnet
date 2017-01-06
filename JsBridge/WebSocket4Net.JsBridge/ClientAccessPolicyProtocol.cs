66c5e4da0c22ff663b0fb08a37db2a4891098aa6
Yuan <Michael.Yuan@bhpbilliton.com>
2017-01-06 13:43:22 +0800

    'all'

---
+++ JsBridge/WebSocket4Net.JsBridge/ClientAccessPolicyProtocol.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+using System.Windows.Browser;
+using System.ComponentModel;
+
+namespace WebSocket4Net.JsBridge
+{
+    class ClientAccessPolicyProtocol
+    {
+        internal const int Http = 0;
+        internal const int Tcp = 1;
+    }
+}
